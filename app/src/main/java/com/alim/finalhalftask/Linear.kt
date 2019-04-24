6c9a6ddd0b5d33a688e9bc62ae46c29975f3c538
yusonafanji <yusronafanji97@gmail.com>
2019-04-24 09:28:39 +0700

    kedua

--- app/src/main/java/com/alim/finalhalftask/Linear.kt
+++ app/src/main/java/com/alim/finalhalftask/Linear.kt
@@ -1,12 +1,37 @@
 package com.alim.finalhalftask
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_linear.*
 
 class Linear : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_linear)
+
+        prof.setOnClickListener {
+            val uri = Uri.parse("https://www.instagram.com/alim.tjr")
+            val i = Intent(Intent.ACTION_VIEW, uri)
+            i.setPackage("com.instagram.android")
+            try {
+                startActivity(i)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://instagram.com/alim.tjr")
+                    )
+                )
+            }
+        }
+        cam.setOnClickListener {  }
+        mowida.setOnClickListener { val intent = Intent(this, Relative::class.java)
+            intent.putExtra("Username","m Alim Tajri")
+            intent.putExtra("password",124123213)
+            startActivity(intent) }
     }
 }
--- app/src/main/java/com/alim/finalhalftask/Relative.kt
+++ app/src/main/java/com/alim/finalhalftask/Relative.kt
@@ -2,11 +2,17 @@ package com.alim.finalhalftask
 
 import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_relative.*
 
 class Relative : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relative)
+
+        val nama=intent.getStringExtra("Username")
+        val password=intent.getIntExtra("password",0)
+        dataterima.setText("User Name ="+ nama + ",\nPassword ="+password)
     }
+
 }
